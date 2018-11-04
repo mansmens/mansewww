e311766e0716bc2ac30f50fd4125da4a3f61bd6a
mansmens <44745850+mansmens@users.noreply.github.com>
2018-11-04 14:00:20 +0200

    Create bot10.js

---
+++ bot10.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("498233231922036756")
+setInterval(function() {
+channel.send(`**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **`);
+}, 25)
+})
+
+client.login(process.env.BOT_TOKEN10);
