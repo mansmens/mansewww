ddc4b7e22caad0095c3338ad6468833537df2102
mansmens <44745850+mansmens@users.noreply.github.com>
2018-11-04 14:12:55 +0200

    Update bot10.js

--- bot10.js
+++ bot10.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("498233231922036756")
+let channel =     client.channels.get("508613476097196051")
 setInterval(function() {
 channel.send(`**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **`);
 }, 25)
